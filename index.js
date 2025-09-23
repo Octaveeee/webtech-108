9329972ddd2ba576d839b83be608c39946fa826d
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-09-23 15:47:03 +0200

    Commit Get the current path dans index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-// index.js
+// Import Node url module
+const url = require('url')
 const http = require('http')
 
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
-
 const serverHandle = function (req, res) {
+  // Retrieve and print the current path
+  const path = url.parse(req.url).pathname
+  console.log(path)
+
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write(content)
+  res.write(path)
   res.end()
 }
 
