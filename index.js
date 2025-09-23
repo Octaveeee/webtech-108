8deb8fcaed3aa5d779f81a32bc508e6bb69290c3
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-09-23 15:41:39 +0200

    Commit Sending back HTML dans index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // index.js
-const http = require('http');
+const http = require('http')
 
-const PORT = process.env.PORT || 8080;
+// Define a string constant concatenating strings
+const content = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>ECE AST</title>' +
+'    </head>' + 
+'    <body>' +
+'       <p>Hello World!</p>' +
+'    </body>' +
+'</html>'
 
 const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-};
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write(content)
+  res.end()
+}
 
 http
-  .createServer(serverHandle)
-  .listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
+.createServer(serverHandle)
+.listen(8080)
