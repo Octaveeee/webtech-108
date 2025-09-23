803a5631246844ea1747c154048adf57fe5bcab6
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-09-23 15:33:53 +0200

    Commit create http server 1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Node.js!");
+// index.js
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const serverHandle = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+};
+
+http
+  .createServer(serverHandle)
+  .listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
