b5daced6562229e57acde89c4cd9e62873ee7ca6
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-09-23 15:54:50 +0200

    Commit Organize the source code dans index.js

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-const url = require('url')
-const qs = require('querystring')
+// ./index.js
 const http = require('http')
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.end()
-}
+const handles = require('./handles')
 
 http
-.createServer(serverHandle)
+.createServer(handles.serverHandle)
 .listen(8080)
