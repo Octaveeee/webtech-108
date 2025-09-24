e6bb75c6998634f21b245b057058e95fb95d1735
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-09-24 13:55:45 +0200

    LAB3 : express API

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+const db = {
+    articles: [
+      {
+        id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        title: 'My article',
+        content: 'Content of the article.',
+        date: '04/10/2022',
+        author: 'Liz Gringer'
+      },
+      // ...
+    ],
+    comments: [
+      {
+        id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+        timestamp: 1664835049,
+        content: 'Content of the comment.',
+        articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        author: 'Bob McLaren'
+      },
+      // ...
+    ]
+  }
+
 // Middleware pour parser JSON si besoin plus tard
 app.use(express.json())
 
@@ -18,6 +41,50 @@ app.get('/hello', (req, res) => {
         res.send('Hello ' + name)
     }
 })
+
+app.get('/articles', (req, res) => {
+    res.json(db.articles)
+})
+
+
+app.post('/articles', (req, res) => {
+    const article = req.body
+    db.articles.push(article)
+    res.status(201).json(article)
+})
+
+app.get('/articles/:articleId', (req, res) => {
+    const article = db.articles.find(a => a.id === req.params.articleId)
+    if (article) {
+        res.json(article)
+      } else {
+        res.status(404).send('Article not found')
+      }
+
+})
+
+
+app.get('/articles/:articleId/comments', (req, res) => {
+    const comments = db.comments.filter(c => c.articleId === req.params.articleId)
+    res.json(comments)
+})
+
+app.post('/articles/:articleId/comments', (req, res) => {
+    const comment = req.body
+    comment.articleId = req.params.articleId
+    db.comments.push(comment)
+    res.status(201).json(comment)
+})
+
+app.get('/articles/:articleId/comments/:commentId', (req, res) => {
+    const comment = db.comments.find(c => c.id === req.params.commentId && c.articleId === req.params.articleId)
+    if (comment) {
+      res.json(comment)
+    } else {
+      res.status(404).send('Comment not found')
+    }
+})
+
 
 // Serveur
 app.listen(port, () => {
