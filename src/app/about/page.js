0b2dd81f1027411aadb0ea49f47096f5db7fb1f9
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-10-06 10:20:24 +0200

    Commit ajout du lab4

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <h2>About</h2>
+      <p>This page was created to share knowledge about modern web technologies.</p>
+      <Footer />
+    </div>
+  )
+}
