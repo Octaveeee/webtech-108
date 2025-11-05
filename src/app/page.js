17f6e3501f5bed4fcbd88ec12f3ff9a717bf399b
Octave SAVEAUX <55949956+Octaveeee@users.noreply.github.com>
2025-11-05 11:46:39 +0100

    perf: optimize texture files for performance

--- src/app/page.js
+++ src/app/page.js
@@ -75,6 +75,15 @@ export default function Home() {
       </main>
 
       <Footer />
+
+      <link rel="preload" as="image" href="/textures/floor.jpg" />
+      <link rel="preload" as="image" href="/textures/wall.jpg" />
+      <link rel="preload" as="image" href="/textures/roof.jpg" />
+      <link rel="preload" as="image" href="/textures/plinth.jpg" />
+
     </div>
+
+
+    
   )
 }
